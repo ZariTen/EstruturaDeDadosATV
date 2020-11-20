f4ff42091b85cc7b79844ba3311eafb5d70b51a2
Wheados <Wheadosz@hotmail.com>
2020-11-20 16:22:37 -0300

    simulador header functions

---
+++ include/simulador.hpp
@@ -0,0 +1,11 @@
+#ifndef SIM_HPP
+#define SIM_HPP
+#include <string>
+
+struct simulador{
+    lerArquivo(std::string nomeArquivo);
+    rodarComando(std::string estrutura, std::string operacao, int parametro, int parametro2);
+};
+
+
+#endif
