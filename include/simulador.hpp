c2ebb1c6ee1522fb5a151d1689a1e6cda722097f
Wheados <Wheadosz@hotmail.com>
2020-11-21 01:26:25 -0300

    missing types on simulador

--- include/simulador.hpp
+++ include/simulador.hpp
@@ -3,8 +3,8 @@
 #include <string>
 
 struct simulador{
-    lerArquivo(std::string nomeArquivo);
-    rodarComando(std::string estrutura, std::string operacao, int parametro, int parametro2);
+    void lerArquivo(std::string nomeArquivo);
+    void rodarComando(std::string estrutura, std::string operacao, int parametro, int parametro2);
 };
 
 
