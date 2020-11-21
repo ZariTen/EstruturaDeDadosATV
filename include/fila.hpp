00e8301d6c6310da987d8409003e12d30bd0764b
Wheados <Wheadosz@hotmail.com>
2020-11-21 13:00:43 -0300

    made fila functions

--- include/fila.hpp
+++ include/fila.hpp
@@ -3,27 +3,88 @@
 
 template <typename T>
 struct fila{
-    
+    struct Node{
+        T value;
+        Node* prev;
+    };
+
+    int count = 0;    
+    Node* first = nullptr;
+    Node* last = nullptr;
 
     int size(){
-        
+       return count; 
     }
 
     void pushBack(T value){
-        
+        if (first != nullptr){
+            Node* other = new Node();
+            other->value = value;
+            other->prev = nullptr;
+
+            last->prev = other;
+            last = other;
+            ++count;
+        } else{
+            first = new Node();
+            first->value = value;
+            first->prev = nullptr;
+            last = first;
+            ++count;
+
+        }
     }
 
-    void popFront();
+    void popFront(){
+        if (first != nullptr){
+            if (first->prev != nullptr){
+                Node* temp = first->prev;
+                delete first;
+                first = temp;
+                --count;
+            } else{
+                delete first;
+                first = nullptr;
+                count = 0;
+            }
+        }
 
-    T front(){
-        
+
     }
 
-    T back();
+    Node * front(){
+        if (first != nullptr){
+            return first; 
+        } else{
+            return nullptr;
+        }
+    }
+
+    Node * back(){
+        if (last != nullptr){
+            return last;
+        } else{
+            return nullptr;
+        }
+
+    }
 
     ~fila(){ //Impedir vazamento de memoria
-        
-        
+        if (count == 0){
+            return;
+        }
+
+        Node* current = first;
+
+        while(current != nullptr){
+            Node* tmp = current->prev;
+            delete current;
+            current = tmp;
+       }
+
+       count = 0;
+       first = nullptr;
+       last = nullptr;
 
     }
 };
