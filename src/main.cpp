b3a72e457f022e0a4823454c3f56055933bf0bbf
Wheados <Wheadosz@hotmail.com>
2020-11-21 13:01:20 -0300

    testes for fila

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <pilha.hpp>
+#include <fila.hpp>
 
 
 int main(int argc, char* argv[]){
-        pilha<int> pilhaTeste;
-        pilhaTeste.push(10);
+        fila<int> filaTeste;
+        filaTeste.pushBack(1);
+        filaTeste.pushBack(2);
 
-        std::cout << pilhaTeste.top() << std::endl;
-        pilhaTeste.pop();
-        std::cout << pilhaTeste.top() << std::endl;
-        pilhaTeste.push(4);
-        std::cout << pilhaTeste.top() << std::endl;
-        
+        std::cout << filaTeste.front()->value << std::endl;
+        std::cout << filaTeste.back()->value << std::endl;
+        filaTeste.popFront();
+        std::cout << filaTeste.front()->value << std::endl;
+        filaTeste.popFront();        
         return 0;
 }
